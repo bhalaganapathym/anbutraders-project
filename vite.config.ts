20ac9805b922e0fce9bf9403178f955cb8683edc
BGM <bhalaganapathym@gmail.com>
2026-08-07 09:29:31 +0530

    new_disp_bill

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/api/v1',
+        target: 'http://localhost:8080/api/v1',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
